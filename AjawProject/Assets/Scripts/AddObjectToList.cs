cddff67152b9671b8cf93b35d24603e19d9bb180
Fran <fran152job@gmail.com>
2019-08-30 13:15:03 +0200

    Humans list

---
+++ AjawProject/Assets/Scripts/AddObjectToList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObjectToList : MonoBehaviour
+{
+    int i = 0;
+    public GameObject itemTemplate;
+    public GameObject content;
+
+    public void AddButtonClick()
+    {
+        var copy = Instantiate(itemTemplate);
+        copy.transform.parent = content.transform;
+
+    }
+}
